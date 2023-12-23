74f39751193917ab581f891d233ea32a6c01ed4c
Magnus Mutai <MagnusMq07@gmail.com>
2023-12-23 11:44:24 +0300

    commit staged if you want

--- obj/Debug/net8.0/AuthGen.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuthGen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthGen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c86d744740ecd135c21e8f90ad1a4c94467ecef9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1825c70748aef51dc8357ac5094d04030971c8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthGen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthGen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
